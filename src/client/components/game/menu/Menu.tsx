4f79d4568f2c8bfac1428364f328d7ea7c1bb3c9
Hunter Parcells <hparcells@icloud.com>
2019-09-08 10:24:06 -0400

    Started Styled Menu

--- src/client/components/game/menu/Menu.tsx
+++ src/client/components/game/menu/Menu.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormGroup from '@material-ui/core/FormGroup';
 
 import socket from '../../../socket';
 
@@ -7,6 +14,18 @@ import { Store } from '../../../store';
 import { toggleCreationWindow, changeRoomCode, joinRoom } from '../../../actions';
 
 import CreationWindow from './CreationWindow';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      padding: theme.spacing(2),
+      margin: theme.spacing(1)
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+);
 
 function Menu(
   {
@@ -22,6 +41,8 @@ function Menu(
     joinRoom: () => void
   }
 ) {
+  const classes = useStyles();
+
   useEffect(() => {
     function handleNonExistentRoom() {
       // TODO:
@@ -40,18 +61,49 @@ function Menu(
 
   return (
     <div>
-      <div>
-        <button onClick={toggleCreationWindow}>Create Room</button>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={6}>
+          <Paper className={classes.paper}>
+            <Typography variant='h5'>Create Room</Typography>
+            <Typography paragraph>Create a room for someone to join.</Typography>
+            <Button
+              variant='contained'
+              color='primary'
+              fullWidth
+              onClick={toggleCreationWindow}
+            >
+              Create Room
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <Paper className={classes.paper}>
+            <Typography variant='h5'>Join Room</Typography>
+            <Typography paragraph>Join a room with a room code.</Typography>
+            <FormGroup row>
+              <TextField
+                label='Room Code'
+                // className={classes.textField}
+                value={roomCode}
+                onChange={handleRoomCodeChange}
+                margin='dense'
+                variant='outlined'
+                fullWidth
+              />
+              <Button
+                variant='contained'
+                color='primary'
+                onClick={joinRoom}
+              >
+                Join
+              </Button>
 
-        <CreationWindow />
-      </div>
+            </FormGroup>
+          </Paper>
+        </Grid>
+      </Grid>
 
-      <br />
-
-      <div>
-        <input type='text' onChange={handleRoomCodeChange} value={roomCode} />
-        <button onClick={joinRoom}>Join Room</button>
-      </div>
+      <CreationWindow />
     </div>
   );
 }
